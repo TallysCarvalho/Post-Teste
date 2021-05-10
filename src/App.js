cd6a7199994d66520f414745a67ce9a37546de42
TallysCarvalho <tallys.roberto@hotmail.com>
2021-05-10 19:51:37 -0300

    fazendo a estilizaçao

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
+import './style.css'
 
 //https://jsonplaceholder.typicode.com/posts
 
 function App() {
-  const [post, setPost] = useState([]);
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     function loadApi() {
@@ -13,7 +14,7 @@ function App() {
         .then((response) => response.json())
         .then((json) => {
           console.log(json);
-          setPost(json)
+          setPosts(json)
         });
     }
     loadApi();
@@ -22,8 +23,21 @@ function App() {
 
 
   return (
-    <div className="App">
-      <h1>teste</h1>
+    <div className="container">
+      <header>
+        <strong>React Posts</strong>
+      </header>
+
+      {posts.map((post)=>{
+        return(
+          <article key={post.id} className="post">
+            <strong>{post.title}</strong>
+            <span>{post.body}</span>
+            
+
+          </article>
+        )
+      })}
     </div>
   );
 }
